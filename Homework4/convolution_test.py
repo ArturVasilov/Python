098b905eadc1305a388fabf50765ac62d1f4385e
Artur Vasilov <artur.vasilov@dz.ru>
2016-03-05 22:43:56 +0300

    fixed homework #4

--- Homework4/convolution_test.py
+++ Homework4/convolution_test.py
@@ -1,8 +1,7 @@
 import unittest
 
+from numba import int16
 from numpy import random
-
-from scipy.io.wavfile import read
 
 from datetime import datetime
 
@@ -14,7 +13,7 @@ def call_fourier(xs, ys):
     fourier = fourier_convolution(xs, ys)
     fourier_list = []
     for value in fourier:
-        fourier_list.append(round(abs(value)))
+        fourier_list.append(int16(round(abs(value))))
     return fourier_list
 
 
@@ -31,37 +30,37 @@ class FourierConvolutionTest(unittest.TestCase):
     def test_fourier_convolution(self):
         xs = [1, 3, 5]
         ys = [4, 2, 1]
-        direct = call_fourier(xs, ys)
+        fourier = call_fourier(xs, ys)
         result = [17, 19, 27]
-        self.assertListEqual(result, direct)
+        self.assertListEqual(result, fourier)
 
     def test_small_fixed_input(self):
         xs = [1, 3, 7, 10, 54, 19, 8, 21]
         ys = [4, 71, 12, 23, 6, 9, 112, 48]
-        direct = call_fourier(xs, ys)
+        direct = direct_convolution(xs, ys)
         fourier = call_fourier(xs, ys)
         self.assertListEqual(direct, fourier)
 
     def test_small_random_set(self):
-        xs = random.rand(50) * 100
-        ys = random.rand(50) * 1000
-        direct = call_fourier(xs, ys)
+        xs = int16(random.rand(10) * 50)
+        ys = int16(random.rand(10) * 50)
+        direct = direct_convolution(xs, ys)
         fourier = call_fourier(xs, ys)
         self.assertListEqual(direct, fourier)
 
     def test_large_random_set(self):
-        xs = random.rand(5000) * 100
-        ys = random.rand(5000) * 1000
-        direct = call_fourier(xs, ys)
+        xs = int16(random.rand(100) * 10)
+        ys = int16(random.rand(100) * 10)
+        direct = direct_convolution(xs, ys)
         fourier = call_fourier(xs, ys)
         self.assertListEqual(direct, fourier)
 
     def test_very_large_random(self):
-        xs = random.rand(500000) * 100
-        ys = random.rand(500000) * 100
+        xs = int16(random.rand(1000) * 10)
+        ys = int16(random.rand(1000) * 10)
 
         start = datetime.now()
-        direct = call_fourier(xs, ys)
+        direct = direct_convolution(xs, ys)
         end = datetime.now()
         direct_time = end - start
 
@@ -71,20 +70,8 @@ class FourierConvolutionTest(unittest.TestCase):
         fourier_time = end - start
 
         self.assertListEqual(direct, fourier)
-        self.assertTrue(fourier_time < direct_time)
+        self.assertTrue(fourier_time < direct_time / 10)
 
-    def test_voice_input(self):
-        name = 'voice.wav'
-        f = open(name, 'rb')
-        # noinspection PyUnusedLocal
-        [fr, dti] = read(f)
-        f.close()
-
-        xs = dti[:10000]
-        ys = dti[10000:20000]
-        direct = call_fourier(xs, ys)
-        fourier = call_fourier(xs, ys)
-        self.assertListEqual(direct, fourier)
 
 if __name__ == '__main__':
     unittest.main()
